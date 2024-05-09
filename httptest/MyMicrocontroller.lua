a82ec89a05b49d788b4528883826535fdfcb8307
shimotuke1535 <simonomao1535@gmail.com>
2024-05-09 18:30:43 +0900

    OK......????

--- httptest/MyMicrocontroller.lua
+++ httptest/MyMicrocontroller.lua
@@ -31,10 +31,10 @@ do
 
         -- NEW! button/slider options from the UI
         simulator:setInputBool(31, simulator:getIsClicked(1))       -- if button 1 is clicked, provide an ON pulse for input.getBool(31)
-        simulator:setInputNumber(1, simulator:getSlider(1)*250)        -- set input 1 to the value of slider 1
-        simulator:setInputNumber(2, simulator:getSlider(2)/2 - 0.25)
+        simulator:setInputNumber(31, simulator:getSlider(1))        -- set input 31 to the value of slider 1
 
         simulator:setInputBool(32, simulator:getIsToggled(2))       -- make button 2 a toggle, for input.getBool(32)
+        simulator:setInputNumber(32, simulator:getSlider(2) * 50)   -- set input 32 to the value from slider 2 * 50
     end;
 end
 ---@endsection
@@ -45,70 +45,14 @@ end
 -- try require("Folder.Filename") to include code from another file in this, so you can store code in libraries
 -- the "LifeBoatAPI" is included by default in /_build/libs/ - you can use require("LifeBoatAPI") to get this, and use all the LifeBoatAPI.<functions>!
 
-w = 0
-h = 0
-Point = {
-    new = function()
-        return
-        {
-            x = 0,
-            y = 0,
-        }
-    end
-};
-point = {}
-sec = 0
 ticks = 0
 function onTick()
-	x0 = w / 2
-	y0 = h - h / 6
-    rng = input.getNumber(1)
-    dig = math.rad(input.getNumber(2) * 360)
-    index = 2.5
-    for i = 0, 140, 1
-    do
-        point[i] = Point.new()
-        point[i].x = 0
-        point[i].y = 0
-    end 
-    for i = 0, 120, 1
-    do
-        point[i].x = x0 - (rng/index) * math.sin(dig)
-        point[i].y = y0 - (rng/index) * math.cos(dig)
-    end
-    Rx = w * math.sin(dig)
-    Ry = w * math.cos(dig)
     ticks = ticks + 1
-    sec = math.ceil(ticks / 60)
-    if sec > 2 then
-        ticks=0
-        for i=0 , 120, 1
-        do
-            point[i].x = 0
-            point[i].y = 0
-        end
-    end
-    i = 0
 end
 
 function onDraw()
-    w = screen.getWidth()
-	h = screen.getHeight()
-	screen.setColor(0,255,0)
-	screen.drawLine(0,y0,w,y0)
-	screen.drawLine(x0,0,x0,h)
-	screen.drawLine(x0,y0,x0-Rx,y0-Ry)
-	for i = 0, 100, 10
-	do
-		screen.drawCircle(x0,y0,i)
-	end
-	screen.setColor(255,0,0)
-	screen.drawText(x0,y0,point[ticks].x)
-	screen.drawText(x0,y0+6,point[ticks].y)
-    screen.drawText(x0,y0+12,sec)
-    for i = 0, 120, 1
-    do
-        screen.drawCircleF(point[ticks].x,point[ticks].y,2)
-    end
-	
+    screen.drawCircle(16,16,5)
 end
+
+
+
