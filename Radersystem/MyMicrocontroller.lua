adcf7530c05aae43cda5a1ec3e9c96b440ce6ac8
shimotuke1535 <simonomao1535@gmail.com>
2024-05-09 11:26:01 +0900

    Add 3 Projects in operating in Life boat

--- Radersystem/MyMicrocontroller.lua
+++ Radersystem/MyMicrocontroller.lua
@@ -14,7 +14,7 @@ do
     ---@type Simulator -- Set properties and screen sizes here - will run once when the script is loaded
     simulator = simulator
     simulator:setScreen(1, "3x3")
-    simulator:setProperty("LimitRange", 1000)
+    simulator:setProperty("ExampleNumberProperty", 123)
 
     -- Runs every tick just before onTick; allows you to simulate the inputs changing
     ---@param simulator Simulator Use simulator:<function>() to set inputs etc.
@@ -22,12 +22,19 @@ do
     function onLBSimulatorTick(simulator, ticks)
 
         -- touchscreen defaults
-        simulator:setInputBool(1, simulator:getIsToggled(1))
-        simulator:setInputNumber(1, simulator:getSlider(1) * 1000)
-        simulator:setInputNumber(2, simulator:getSlider(2) * (2 * math.pi))
-        simulator:setInputNumber(3, simulator:getSlider(3))
-        simulator:setInputNumber(4, simulator:getSlider(4) * 20)
-        simulator:setInputNumber(32, simulator:getSlider(5) * 100)
+        local screenConnection = simulator:getTouchScreen(1)
+        simulator:setInputBool(1, screenConnection.isTouched)
+        simulator:setInputNumber(1, screenConnection.width)
+        simulator:setInputNumber(2, screenConnection.height)
+        simulator:setInputNumber(3, screenConnection.touchX)
+        simulator:setInputNumber(4, screenConnection.touchY)
+
+        -- NEW! button/slider options from the UI
+        simulator:setInputBool(31, simulator:getIsClicked(1))       -- if button 1 is clicked, provide an ON pulse for input.getBool(31)
+        simulator:setInputNumber(31, simulator:getSlider(1))        -- set input 31 to the value of slider 1
+
+        simulator:setInputBool(32, simulator:getIsToggled(2))       -- make button 2 a toggle, for input.getBool(32)
+        simulator:setInputNumber(32, simulator:getSlider(2) * 50)   -- set input 32 to the value from slider 2 * 50
     end;
 end
 ---@endsection
--- timetest/MyMicrocontroller.lua
+++ timetest/MyMicrocontroller.lua
@@ -44,51 +44,17 @@ end
 
 -- try require("Folder.Filename") to include code from another file in this, so you can store code in libraries
 -- the "LifeBoatAPI" is included by default in /_build/libs/ - you can use require("LifeBoatAPI") to get this, and use all the LifeBoatAPI.<functions>!
-w = 0
-h = 0
-Point = {
-    new = function()
-        return
-        {
-            x = 0,
-            y = 0,
-        }
-    end
-};
-point = {}
-t = 0
-function onTick()
-    rng = input.getNumber(1)
-    dig = math.rad(input.getNumber(2) * 360)
-    index = 2.5
-    point[t] = Point.new()
-    point[t].x = (rng/index) * math.sin(dig)
-    point[t].y = (rng/index) * math.cos(dig)
-    Rx = w * math.sin(dig)
-    Ry = w * math.cos(dig)
-    t = t + 1
-    if t >= 120 then
-        t=0
-    end
-end
 
+ticks = 0
+function onTick()
+    ticks = ticks + 1
+    sec = math.floor(ticks / 60)
+end
 function onDraw()
+    h = screen.getHeight()
     w = screen.getWidth()
-	h = screen.getHeight()
-	x0 = w / 2
-	y0 = h - h / 6
-	screen.setColor(0,255,0)
-	screen.drawLine(0,y0,w,y0)
-	screen.drawLine(x0,0,x0,h)
-	screen.drawLine(x0,y0,x0-Rx,y0-Ry)
-	for i = 0, 100, 10
-	do
-		screen.drawCircle(x0,y0,i)
-	end
-	screen.setColor(255,0,0)
-	screen.drawText(x0,y0+6,point[t].x)
-	screen.drawText(x0,y0+12,point[t].y)
-	screen.drawCircleF(point[t].x,point[t].y,2)
+    screen.drawCircle(16,16,5)
+    screen.drawText(h/2,w/2,sec)
 end
 
 
