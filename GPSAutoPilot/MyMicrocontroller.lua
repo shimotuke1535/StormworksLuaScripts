2c2fa50029e3827905ebe1c26e01308e8ba357f7
shimotuke1535 <simonomao1535@gmail.com>
2024-06-07 15:53:05 +0900

    add Auto pilots coding now

--- GPSAutoPilot/MyMicrocontroller.lua
+++ GPSAutoPilot/MyMicrocontroller.lua
@@ -45,13 +45,87 @@ end
 -- try require("Folder.Filename") to include code from another file in this, so you can store code in libraries
 -- the "LifeBoatAPI" is included by default in /_build/libs/ - you can use require("LifeBoatAPI") to get this, and use all the LifeBoatAPI.<functions>!
 
-ticks = 0
-function onTick()
-    ticks = ticks + 1
+tick = 0
+Target_value = 0.0
+--目標位置格納テーブル定義
+Target = 
+{
+    new = function()
+        return
+        {
+            x = 0.0,
+            y = 0.0
+        }
+    end
+}
+--現在位置・方位格納テーブル定義
+Current = 
+{
+    new = function()
+        
+        return
+        {
+            x = 0.0,
+            y = 0.0,
+            dir = 0.0
+        }
+    end
+}
+--差分位置・方位格納テーブル
+dif = 
+{
+    new = function()
+        return
+        {
+            x = 0.0,
+            y = 0.0,
+            dir = 0.0
+        }
+    end
+}
+--PIDコントローラー
+function PID_controller(Kp, Ki, Kd, C_value, PreC_value, T)
+    Controll_value = 0.0
+    e = 0 - C_value
+    de = (e - PreC_value) / T
+    ie = ie + (e - PreC_value) * T/2
+    Controll_value = Kp * e + Ki * ie + Kd * de
+    return Controll_value
+end
+function ontick()
+    --変数定義
+    distance = 0.0
+    pre_distance = 0.0
+    direction = 0.0
+    pre_direction = 0.0
+    throtlle = 0.0
+    Ladder = 0.0
+    --目標位置取得
+    Target = Target.new()
+    Target.x = input.getNumber(1)
+    Target.y = input.getNumber(2)
+    --現在位置・方位取得
+    Current = Current.new()
+    Current.x = input.getNumber(3)
+    Current.y = input.getNumber(4)
+    Current.dir = input.getNumber(5)
+    --差分位置・方位計算
+    diff = dif.new()
+    diff.x = Target.x - Current.x
+    diff.y = Target.y - Current.y
+    diff.dir = math.atan(diff.y, diff.x) - Current.dir
+    --PID制御
+    throtlle = PID_controller(1, 1, 1, distance, pre_distance, tick)
+    Ladder  = PID_controller(1, 1, 1, direction, pre_direction, tick)
+    --出力
+    output.setNumber(10, throtlle)
+    output.setNumber(11, Ladder)
+    --tick加算
+    tick = tick + 1
 end
 
 function onDraw()
-    screen.drawCircle(16,16,5)
+    
 end
 
 
